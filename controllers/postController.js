d63879c70830dee57e706dde0381d7608037e62b
Dinesh <dineshlegha23@gmail.com>
2024-09-20 20:53:48 +0530

    added user post controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,21 @@
 import prisma from "../prisma/prismaClient.js";
+import verifyToken from "../utils/verifyToken.js";
 
 const getPosts = async (req, res) => {
+  const query = req.query;
   try {
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+      where: {
+        city: query.city || undefined,
+        type: query.type || undefined,
+        property: query.property || undefined,
+        bedroom: parseInt(query.bedroom) || undefined,
+        price: {
+          gte: parseInt(query.minPrice) || 0,
+          lte: parseInt(query.maxPrice) || 99999,
+        },
+      },
+    });
     res.status(200).json({ total: posts.length, posts });
   } catch (error) {
     console.log(error);
@@ -12,26 +25,48 @@ const getPosts = async (req, res) => {
 
 const getPost = async (req, res) => {
   const { id } = req.params;
-
-  try {
-    const post = await prisma.post.findUnique({
-      where: { id },
-      include: {
-        postDetail: true,
-        user: {
-          select: { username: true, avatar: true },
+  const { token } = req.cookies;
+  if (token) {
+    try {
+      const { userId } = verifyToken(token);
+      const post = await prisma.post.findUnique({
+        where: { id },
+        include: {
+          postDetail: true,
+          user: {
+            select: { username: true, avatar: true },
+          },
         },
-      },
-    });
+      });
+      const saved = await prisma.savedPost.findUnique({
+        where: { userId_postId: { userId, postId: id } },
+      });
 
-    if (!post) {
-      return res.status(404).json({ msg: "No post found with this id" });
+      return res.status(200).json({ ...post, isSaved: saved ? true : false });
+    } catch (error) {
+      console.log(error);
     }
+  } else {
+    try {
+      const post = await prisma.post.findUnique({
+        where: { id },
+        include: {
+          postDetail: true,
+          user: {
+            select: { username: true, avatar: true },
+          },
+        },
+      });
 
-    return res.status(200).json(post);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ msg: "Failed to get post" });
+      if (!post) {
+        return res.status(404).json({ msg: "No post found with this id" });
+      }
+
+      return res.status(200).json({ ...post, isSaved: false });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ msg: "Failed to get post" });
+    }
   }
 };
 
@@ -82,4 +117,60 @@ const deletePost = async (req, res) => {
   }
 };
 
-export { getPosts, addPost, getPost, updatePost, deletePost };
+const savePost = async (req, res) => {
+  const { userId } = req.user;
+  const { id: postId } = req.params;
+
+  try {
+    const savedPost = await prisma.savedPost.findUnique({
+      where: { userId_postId: { userId, postId } },
+    });
+    if (savedPost) {
+      await prisma.savedPost.delete({
+        where: { id: savedPost.id },
+      });
+      res.status(200).json({ msg: "Post unsaved" });
+    } else {
+      await prisma.savedPost.create({
+        data: {
+          userId,
+          postId,
+        },
+      });
+      res.status(201).json({ msg: "Post saved" });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Failed to save/unsave post" });
+  }
+};
+
+const userPosts = async (req, res) => {
+  const { userId } = req.user;
+
+  try {
+    const posts = await prisma.post.findMany({
+      where: { userId },
+    });
+
+    let savedPosts = await prisma.savedPost.findMany({
+      where: { userId },
+      include: { post: true },
+    });
+
+    savedPosts = savedPosts.map((post) => post.post);
+
+    res.status(200).json({ posts, savedPosts });
+  } catch (error) {
+    res.status(500).json({ msg: "Failed to get user posts" });
+  }
+};
+
+export {
+  getPosts,
+  addPost,
+  getPost,
+  updatePost,
+  deletePost,
+  savePost,
+  userPosts,
+};
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,15 +5,19 @@ import {
   getPost,
   updatePost,
   deletePost,
+  savePost,
+  userPosts,
 } from "../controllers/postController.js";
 import authentication from "../middlewares/authentication.js";
 const router = express.Router();
 
 router.route("/").get(getPosts).post(authentication, addPost);
+router.get("/users/posts", authentication, userPosts);
 router
   .route("/:id")
   .get(getPost)
   .put(authentication, updatePost)
-  .delete(authentication, deletePost);
+  .delete(authentication, deletePost)
+  .post(authentication, savePost);
 
 export default router;
