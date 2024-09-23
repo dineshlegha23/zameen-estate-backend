d6493015567e3cd411d9d5fe497ef35b90e79daa
Dinesh <dineshlegha23@gmail.com>
2024-09-23 16:02:41 +0530

    changed cookie settings

--- middlewares/attachCookiesToResponse.js
+++ middlewares/attachCookiesToResponse.js
@@ -5,6 +5,8 @@ const attachCookiesToResponse = (userId, res) => {
   return res.cookie("token", token, {
     httpOnly: true,
     sameSite: "None",
+    secure: process.env.PROD_CLIENT_URL || false,
+    domain: process.env.PROD_CLIENT_URL || process.env.CLIENT_URL,
   });
 };
 
