e4b4c9cf9f7dbeffda5220f5b14310700dc2b4f5
Dinesh <dineshlegha23@gmail.com>
2024-09-23 16:23:37 +0530

    fixed cookie

--- middlewares/attachCookiesToResponse.js
+++ middlewares/attachCookiesToResponse.js
@@ -4,9 +4,7 @@ const attachCookiesToResponse = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY);
   return res.cookie("token", token, {
     httpOnly: true,
-    sameSite: "None",
-    secure: process.env.PROD_CLIENT_URL || false,
-    domain: process.env.PROD_CLIENT_URL || process.env.CLIENT_URL,
+    sameSite: "none",
   });
 };
 
