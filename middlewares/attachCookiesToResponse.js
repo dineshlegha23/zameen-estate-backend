ae2217a44ec36f9d20f528f10a36e80fadae83ad
Dinesh <dineshlegha23@gmail.com>
2024-09-23 15:55:37 +0530

    added samesite to none in cookie

--- middlewares/attachCookiesToResponse.js
+++ middlewares/attachCookiesToResponse.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken";
 
 const attachCookiesToResponse = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY);
-  return res.cookie("token", token, { httpOnly: true });
+  return res.cookie("token", token, {
+    httpOnly: true,
+    sameSite: "None",
+  });
 };
 
 export default attachCookiesToResponse;
